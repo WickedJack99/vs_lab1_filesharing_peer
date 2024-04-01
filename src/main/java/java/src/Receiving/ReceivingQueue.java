bc0282c851b3471d80a0ba5d84bf7da0b9673c35
WickedJack99 <wickedjack1@proton.me>
2024-04-01 17:28:01 +0200

    Receive part classes.

---
+++ src/main/java/java/src/Receiving/ReceivingQueue.java
@@ -0,0 +1,9 @@
+/**
+ * @author Aaron Moser
+ */
+
+package java.src.Receiving;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ReceivingQueue extends LinkedBlockingQueue<ReceivedData> {}
