d8a6174a9d53c8b83b53aad33006038d114ccf4f
WickedJack99 <wickedjack1@proton.me>
2024-04-01 17:23:55 +0200

    Classes part of sending logic.

---
+++ src/main/java/java/src/Sending/SenderThread.java
@@ -0,0 +1,59 @@
+/**
+ * @author Aaron Moser
+ */
+
+package java.src.Sending;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.src.Terminatable;
+import java.src.Sending.Data.Sendable;
+
+public class SenderThread extends Thread implements Terminatable {
+
+    private DatagramSocket ds = null;
+    private SendingQueue sendingQueue = null;
+
+    public SenderThread(DatagramSocket ds, SendingQueue sendingQueue) {
+        this.ds = ds;
+        this.sendingQueue = sendingQueue;
+    }
+
+    private boolean senderThreadRunning = true;
+
+    @Override
+    public void run() {
+        while (senderThreadRunning) {
+            try {
+                Sendable dataToSent = sendingQueue.take();
+
+                byte[] fileData = dataToSent.getMessage();
+
+                if (fileData != null) {
+                    String receiverIpAddress = dataToSent.getReceiver().ipAddress();
+                    int receiverPort = dataToSent.getReceiver().port();
+                    int fileDataLength = fileData.length;
+
+                    DatagramPacket dp =
+                        new DatagramPacket(fileData, fileDataLength, InetAddress.getByName(receiverIpAddress), receiverPort);
+                    ds.send(dp);
+                }
+            } catch (UnknownHostException e) {
+                System.out.println("Error, host is unknown or unreachable.");
+            } catch (IOException e) {
+                System.out.println("Error, wasn't able to send data, because of problem with socket.");
+            } catch (InterruptedException e) {
+                System.out.println("Error, sending queue was interrupted. Terminating (this) sending thread..");
+                this.terminate();
+            }
+        }
+    }
+
+    public void terminate() {
+        senderThreadRunning = false;
+    }
+}
+
