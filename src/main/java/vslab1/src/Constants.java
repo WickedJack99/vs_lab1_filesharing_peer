71dd04ce1fdca60ae437501c4792ed557cb7dd57
WickedJack99 <wickedjack1@proton.me>
2024-04-04 22:14:16 +0200

    File to administrate centralized constants.

---
+++ src/main/java/vslab1/src/Constants.java
@@ -0,0 +1,6 @@
+package vslab1.src;
+
+public class Constants {
+    public static final String PEERCONFIGFILENAME = ".peers.json";
+    public static final String PEERCONFIGFILEPATH = ".filesharing_data"; 
+}
