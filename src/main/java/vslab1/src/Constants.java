d43abad0cbea9d7ce594f8b5eb22eda542b97c58
WickedJack99 <wickedjack1@proton.me>
2024-04-07 11:52:49 +0200

    Removed test peers from config init string.

--- src/main/java/vslab1/src/Constants.java
+++ src/main/java/vslab1/src/Constants.java
@@ -6,8 +6,5 @@ public class Constants {
 
     public static final String PEERCONFIGFILEINITIALCONTENT = 
         "{\"peer\":{\"ipAddress\":\"\",\"port\":0,\"files\":[]}," + 
-         "\"peers\":[" + 
-            "{\"ipAddress\":\"127.0.0.1\",\"port\":5001,\"files\":[],\"onlineState\":\"offline\"}" + 
-            ",{\"ipAddress\":\"127.0.0.1\",\"port\":5002,\"files\":[],\"onlineState\":\"offline\"}" +
-         "]}";
+         "\"peers\":[]}";
 }
