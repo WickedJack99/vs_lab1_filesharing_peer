e647f69acb6eef18b0e2896f6eff812e8340039b
WickedJack99 <wickedjack1@proton.me>
2024-04-07 11:55:52 +0200

    Added function to convert to JSONObject at ReceivedData class.

--- src/main/java/vslab1/src/Receiving/ReceivedData.java
+++ src/main/java/vslab1/src/Receiving/ReceivedData.java
@@ -4,4 +4,20 @@
 
 package vslab1.src.Receiving;
 
-public record ReceivedData(String data) {}
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public record ReceivedData(String data) {
+
+    public JSONObject interpretAsJSONObject() {
+        try {
+            JSONObject receivedDataAsJSONObject = null;
+            receivedDataAsJSONObject = new JSONObject(data);
+            return receivedDataAsJSONObject;
+        } catch (JSONException e) {
+            System.out.println(data);
+            System.err.println("File is a json file, not able to parse it.");
+        }
+        return null;
+    }
+}
